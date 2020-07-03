5a76fc9315ee2b05737a6f88a22c285bb805ea29
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-03 08:28:12 +0300

    Add source.html file, create source view function

--- app/views.py
+++ app/views.py
@@ -6,4 +6,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html') 
+
+    message = 'Hello People'
+    return render_template('index.html',message = message) 
+
+@app.route('/source/<int:source_id>')
+def source(source_id):
+    '''
+    View source page function that returns the articles page and its data
+    '''
+    return render_template('source.html',id = source_id)    

4b94edf4604aed656bb09fe7f188b864f180c832
Makena-Maryann <maryann.makena00@gmail.com>
2020-07-05 11:14:43 +0300

    Create tests, create Source class

---
+++ app/models/source.py
@@ -0,0 +1,13 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+    def __init__(self,id,name,description,url,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.country = country
+
+
+    
---
+++ app/test_source.py
@@ -0,0 +1,34 @@
+import unittest
+from models import source
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the Sources class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_source = Source('abc-news', 'ABC News', 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.', 'https://abcnews.go.com', 'us')
+
+    def test_instance(self):
+        '''
+        test that checks if our object is an instance of the Source class
+        '''
+        self.assertTrue(isinstance(self.new_source,Source)) 
+
+    def test_init(self):
+        '''
+        test case to test if the object is instantiated correctly
+        '''
+        self.assertEqual(self.new_source.id,'abc-news')
+        self.assertEqual(self.new_source.name,'ABC News')
+        self.assertEqual(self.new_source.description,'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.')
+        self.assertEqual(self.new_source.url,'https://abcnews.go.com')
+        self.assertEqual(self.new_source.country,'us')
+               
+
+if __name__=='__main__':
+    unittest.main()        
